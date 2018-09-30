04d1761837aa717cb7420aca4432257df86e8922
aleks <akonovalcev@gmail.com>
2018-09-30 14:41:21 +0400

    * Lesson1_Stream4.sln: Completed Task14
    * Program.cs:
    * Task14.csproj:

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace Task4
+// * Автоморфные числа. Натуральное число называется автоморфным, если оно равно последним цифрам своего квадрата.
+// Например, 25 ^ 2 = 625.
+// Напишите программу, которая вводит натуральное число N и выводит на экран все автоморфные числа, не превосходящие N.
+
+namespace Lesson1_Stream4
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p = new Program();
+            var N = p.AskUser();
+            Console.WriteLine($"Number is automorphic: {p.IsAutomorphic(N)}");
+        }
+
+        int AskUser()
+        {
+            int result = 0;
+            Console.WriteLine("Enter N");
+            try
+            {
+                result = int.TryParse(Console.ReadLine(), out result) ? result : throw new Exception("Incorrect value, please, enter integer N");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+
+        bool IsAutomorphic(int N)
+        {
+            var a = N * N;
+            while (N > 0)
+            {
+                if (N % 10 != a % 10) return false;
+                N /= 10;
+                a /= 10;
+            }
+            return true;
         }
     }
 }
